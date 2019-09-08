cca7d4c0edf1fb8d9ada3322873f0650527c4691
league <league@imac-18.attlocal.net>
2019-09-08 14:30:07 -0700

    kf

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -33,24 +33,45 @@ public class StringIntegerPair {
 		//C. create an Integer array that is one element longer than values
 		Integer[] newValues = new Integer[values.length + 1];
 		//D. set the last element of the new String array to the passed in key
-		
+		newKeys[keys.length + 1] = keys[keys.length];
 		//E. set the last element of the new int array to the passed in value
-		
+		newValues[values.length + 1] = values[values.length];
 		//F. iterate through the keys and values and copy the elements to the new arrays
-		
+		for (int i = 0; i < values.length; i++) {
+			newValues[i] = values[i];
+			newKeys[i] = keys[i];
+		}
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
+		keys = newKeys;
+		values = newValues;
 	}
 	
 	//5. Complete the method so it returns the value located at the passed in key.
 	//   If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+		
+		int placeHolder = -1;
+		for (int i = 0; i < keys.length; i++) {
+			if(key.equals(keys[i])) {
+				placeHolder = i;
+			}
+		}
+		if(placeHolder == -1) {
+			return Integer.MIN_VALUE;
+		}else {
+			return values[placeHolder];
+		}
 	}
 	
 	//6. Complete the containsKey method so that it returns true if the
 	//   passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
 		
+		for (int i = 0; i < keys.length; i++) {
+			if (key.equals(keys[i])) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
@@ -58,12 +79,17 @@ public class StringIntegerPair {
 	//   passed in value is contained in the values array
 	public boolean containsValue(int value) {
 		
+		for (int i = 0; i < values.length; i++) {
+			if (value == values[i]) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+		return keys;
 	}
 	
 	//9. Complete the getValues so it returns the values as an array
